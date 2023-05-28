18c098731fce89a7adf245c6fcc00e61a4b656d5
palmierimayra <palmierimayra@gmail.com>
2023-05-28 12:24:37 -0300

    App

--- Server/src/utils/data.js
+++ Server/src/utils/data.js
@@ -1,17 +1,4 @@
-export const Rick = {
-   id: 1,
-   name: 'Rick Sanchez',
-   status: 'Alive',
-   species: 'Human',
-   gender: 'Male',
-   origin: {
-      name: 'Earth (C-137)',
-      url: 'https://rickandmortyapi.com/api/location/1',
-   },
-   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-};
-
-export default [
+module.exports = [
    {
       id: 1,
       name: 'Rick Sanchez',
