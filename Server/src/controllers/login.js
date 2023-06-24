947d4f0361ed67a3e7dd9e154ce525c3e65814c4
palmierimayra <palmierimayra@gmail.com>
2023-06-24 12:01:22 -0300

    RYM

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,29 +1,28 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
-const login = async (req,res) => {
-    const {email,password} = req.query;
+const login = async (req, res) => {
+  const { email, password } = req.query;
 
-    try {
-        if(!email || !password){
-            res.status(400).send("Faltan datos");
-        } else {
-            const [user] = await User.findOne({where: {email:email}});
-
-            if(!user){
-                res.status(404).send("Usuario no encontrado");
-            } else {
-                if(user.password !== password) {
-                    res.status(403).send("Contraseña incorrecta");
-                } else {
-                    res.status(200).json({access: true});
-                }
-            }
-        }
-    } catch (error) {
-        res.status(500).send(error.message);
+  try {
+    if (!email || !password) {
+      res.status(400).send("Faltan datos");
     }
+    
+    const [user] = await User.findOne({ where: { email: email } });
+
+    if (!user) {
+      res.status(404).send("Usuario no encontrado");
+    }
+    if (user.password !== password) {
+      res.status(403).send("Contraseña incorrecta");
+    }
+
+    res.status(200).json({ access: true });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 };
 
 module.exports = {
-    login,
-}
+  login,
+};
